d608c96892419a8e5383d92ad6acf4c6ee351726
Mehul Kar <mehul.kar@gmail.com>
2012-03-06 12:31:34 -0800

    Added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 RogerJonUrlShortener::Application.routes.draw do
+  
+  
+  devise_for :users
+
+  root :to => "home#index"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
