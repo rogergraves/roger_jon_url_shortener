0fc83099953fea6b90c813e3c8f1cd4fcce81516
Mehul Kar <mehul.kar@gmail.com>
2012-03-06 11:58:47 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RogerJonUrlShortener::Application.config.secret_token = '9f5324914ef703ae63251d533a9edbcd37b514afe86eabe26cf959ecf42542424f0138ed4af81f7db65cea3397da74a1c472d80c9599345e82a35fdc6d061fe6'
