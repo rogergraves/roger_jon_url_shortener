408469536bb1dd3ebb89d684b886dcec6cf3e5e6
Roger Graves <rogergraves@gmail.com>
2012-03-07 21:22:56 -0800

    Add random shorturl generation and http:// prefix adding to Url model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,18 @@ class HomeController < ApplicationController
   
   def create
     logger.info "HomeController.create CALLED!!!!!!!!!!!!!!!!!!"
+    Url.create(user_id: current_user.id, longurl: params[:url])
+    
     render "index"
   end
   
 end
+
+
+# create_table "urls", :force => true do |t|
+#   t.string   "shorturl"
+#   t.string   "longurl"
+#   t.integer  "user_id"
+#   t.datetime "created_at", :null => false
+#   t.datetime "updated_at", :null => false
+# end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,35 @@
 class Url < ActiveRecord::Base
-  validates :shorturl, :longurl, :presence => true
-  validates :shorturl, :uniqueness => true
+  validates :longurl, :user_id, :presence => true
+  
+  before_create :add_http, :add_random_shorturl
+  
+  private
+  
+  def add_http
+    if self.longurl.downcase.start_with?('http') == false
+      self.longurl = 'http://'+self.longurl
+    end
+    
+    logger.info "Url.remove_http TRIGGERED: #{self.longurl}"
+    
+  end
+  
+  def add_random_shorturl
+    logger.info "Url.generate_custom_shorturl IS WORKING!!!!"
+    
+    shortened_url = nil
+    while(shortened_url.nil?)
+      length = rand(4..6)
+      o =  [('a'..'z'),(0..9)].map{|i| i.to_a}.flatten  
+      random_string  =  (0..length).map{ o[rand(o.length)]  }.join
+      
+      shortened_url = random_string unless Url.where("shorturl = ?", random_string).exists?
+    end
+    
+    self.shorturl = shortened_url
+    
+  end
+  
 end
 
 
